5c208de03a64d12089f2a518805f5d2efdbcca23
Nick Gauthier <ngauthier@sabersho.com>
2023-12-06 03:06:16 -0500

    finally

--- Day5/main.py
+++ Day5/main.py
@@ -6,7 +6,7 @@ def lookupmap (source,maplist):
            return m[0] + (source - m[1])        
     return source
 
-with open('./Day5/sampleinput.txt') as f:
+with open('./Day5/input.txt') as f:
     input = f.read().strip().split('\n')
     cleaninput = [i for i in input if i]
 
@@ -57,26 +57,30 @@ print(min(locations))
 
 def lookupmapranges (sources,maplist):
     newranges = []
-    for source in sources:
-        for m in maplist:
-            if source[0] in range(m[1],m[1]+m[2]):
-                if source[-1] in range(m[1],m[1]+m[2]):
-                    newrange = range(m[0] + source[0] - m[1], m[0] + source[0] - m[1] + len(source))
-                    print(newrange)
-                    newranges.append(range(m[0] + source[0] - m[1], m[0] + source[0] - m[1] + len(source)))
-                else:
-                    #uh oh, we have to split a range, first get the beginning chunk
-                    newranges.append(range(m[0] + source[0] - m[1], m[0] + m[2]))
-                    newranges.append(range(source[-1] - m[2],source[-1] + 1))
-    if len(newranges) == 0:
-        newranges.append(range(source[0],source[-1]+1))
-    print(newranges)
-    return newranges
+    for m in maplist:
+        source_start = m[1]
+        source_end = m[1]+m[2]
+        destination_start = m[0]
 
-#seedstemp = range(55,68)
+        tempranges = []
+        while sources:
+            tempsource = sources.pop()
+            left = (tempsource[0],min(tempsource[-1],source_start))
+            mid = (max(tempsource[0],source_start),min(source_end,tempsource[-1]))
+            right = (max(source_end, tempsource[0]),tempsource[-1])
+
+            if left[-1] > left[0]:
+                tempranges.append(left)
+            if mid[-1] > mid[0]:
+                newranges.append((mid[0]-source_start+destination_start,mid[1]-source_start+destination_start))
+            if right[-1] > right[0]:
+                tempranges.append(right)
+        sources = tempranges
+    return newranges + tempranges
+
 seedstemp = []
 for s in range(0,len(seeds),2):
-    seedstemp.append(range(seeds[s],seeds[s]+seeds[s+1]))
+    seedstemp.append((seeds[s],seeds[s]+seeds[s+1]))
 soilranges = []
 fertilizerranges = []
 waterranges = []
@@ -84,17 +88,27 @@ lightranges = []
 tempranges = []
 humidityranges = []
 locationranges = []
+result= []
 
 for seedrange in seedstemp:
+#    print('Seed Range' + str(seedrange))
     soilranges += lookupmapranges([seedrange],seed2soil)
+#   print('Soil Ranges' + str(soilranges))
     fertilizerranges += lookupmapranges(soilranges,soil2fertilizer)
+#    print('Fertilizer Ranges' + str(fertilizerranges))
     waterranges += lookupmapranges(fertilizerranges,fertilizer2water)
+#    print('Water Ranges' + str(waterranges))
     lightranges += lookupmapranges(waterranges,water2light)
+#    print('Light Ranges' + str(lightranges))
     tempranges += lookupmapranges(lightranges,light2temp)
+#    print('Temp Ranges' + str(tempranges))
     humidityranges += lookupmapranges(tempranges,temp2humidity)
+#    print('Humidity Ranges' + str(humidityranges))
     locationranges += lookupmapranges(humidityranges,humidity2location)
+#    print('Location Ranges' + str(locationranges))
 
-print(locationranges)
+print(min([x[0] for x in locationranges]))
+
 #### HAHAHA, this might work, but its killing my computer! need to find more efficient solution
 #seedstemp = []
 #locations2 = []
@@ -115,51 +129,3 @@ print(locationranges)
 #    locations2.append(location)
 #    
 #print(min(locations2))
-
-##### Lets work backwards, find minimum location range and work backwards.
-### DEST START, SOURCE START, RANGE
-# max the source can be to minimize the dest
-
-#minlocation = list(range(0,min([locs[0] for locs in humidity2location])+1))
-#minhumidity = list(range(0,min([locs[0] for locs in temp2humidity])+1))
-#mintemp = list(range(0,min([locs[0] for locs in light2temp])+1))
-#minlight = list(range(0,min([locs[0] for locs in water2light])+1))
-#minwater = list(range(0,min([locs[0] for locs in fertilizer2water])+1))
-#minfertilizer = list(range(0,min([locs[0] for locs in soil2fertilizer])+1))
-#minsoil = list(range(0,min([locs[0] for locs in seed2soil])+1))
-
-#locationmaxrange = min([(i[0],i[1]+i[2]-1) for i in humidity2location]) #for each line in h2l, return the dest start and the source start + range, then mimize on dest start
-#humiditymaxrange = min([(i[0],i[1]+i[2]-1) for i in temp2humidity])
-#tempmaxrange = min([(i[0],i[1]+i[2]-1) for i in light2temp])
-#lightmaxrange = min([(i[0],i[1]+i[2]-1) for i in water2light])
-#watermaxrange = min([(i[0],i[1]+i[2]-1) for i in fertilizer2water])
-#fertilizermaxrange = min([(i[0],i[1]+i[2]-1) for i in soil2fertilizer])
-#soilmaxrange = min([(i[0],i[1]+i[2]-1) for i in seed2soil])
-###
-#location = lookupmap(695457243, humidity2location)
-
-###
-#seedstemp = []
-#for s in range(0,len(seeds),2):
-#    seedstemp.append(i for i in range(seeds[s],seeds[s]+seeds[s+1]))
-
-
-#possibleseeds = []
-#for seed in seeds:
-#    if seed in range(soilmaxrange[0],soilmaxrange[1]):
-#        possibleseeds.append(seed)
-
-
-#locations = []
-#for seed in seedstemp[1]:
-#    soil = lookupmap(seed,seed2soil)
-#    fertilizer = lookupmap(soil,soil2fertilizer)
-#    water = lookupmap(fertilizer,fertilizer2water)
-#    light = lookupmap(water, water2light)
-#    temp = lookupmap(light,light2temp)
-#    humidity = lookupmap(temp,temp2humidity)
-#    location = lookupmap(humidity,humidity2location)
-#
-#    locations.append(location)
-#    
-#print(min(locations))
