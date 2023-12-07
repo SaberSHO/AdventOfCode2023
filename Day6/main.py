dc39c8e1d1ba28e9110be41dddabaeb2800d7341
Nick Gauthier <ngauthier@sabersho.com>
2023-12-07 11:43:48 -0500

    Day 6

---
+++ Day6/main.py
@@ -0,0 +1,53 @@
+import numpy as np
+import regex as re
+import math as math
+
+with open('./Day6/sampleinput.txt') as f:
+    lines = f.read().strip().split('\n')
+    line = [line.split(': ')[1] for line in lines]
+    time = list(map(int,re.findall('\d+',line[0])))
+    distance = list(map(int,re.findall('\d+',line[1])))
+
+# x = hold button
+# y = total time
+# distance(x,y) = x*(y-x) = xy - x2
+def distancefunc(x,y):
+    distance = int(x*y - pow(x,2))
+    return distance
+
+# part1
+winners = []
+for t,d in zip(time, distance):
+    y = t
+    winning = 0
+    for x in range(0,y):
+        disttest = distancefunc(x,y)
+        if disttest > d:
+            winning += 1
+    winners.append(winning)
+
+print(math.prod(winners))
+
+# part2
+t = time2 = int("".join(map(str,time)))
+print(time2)
+d =distance2 = int("".join(map(str,distance)))
+print(distance2)
+
+# BRUTE FORCE METHOD
+winning = 0
+for x in range(0,time2):
+    disttest = distancefunc(x,time2)
+    if disttest > distance2:
+        winning += 1
+print(winning)
+
+#LETS BE SMART
+
+b2m4ac = (t**2) - (4*-1*d)
+sol1 = (-t-math.sqrt(b2m4ac))/(2)
+sol2 = (-t+math.sqrt(b2m4ac))/(2)
+print(sol1)
+print(sol2)
+
+print(distancefunc(math.floor(sol2),9))
